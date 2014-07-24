e1cc2b6afb8251df13c2a01b88da8f6a9fc7dd08
Zhaoxiu Zeng <zhaoxiu.zeng@gmail.com>
2014-07-24 15:04:38 +0800

    ieee80211_is_empty_essid: remove lots of if ... else ..., instead of constant array

--- core/rtw_ieee80211.c
+++ core/rtw_ieee80211.c
@@ -1956,69 +1956,43 @@ void rtw_get_bcn_info(struct wlan_network *pnetwork)
 //show MCS rate, unit: 100Kbps
 u16 rtw_mcs_rate(u8 rf_type, u8 bw_40MHz, u8 short_GI_20, u8 short_GI_40, unsigned char * MCS_rate)
 {
-	u16 max_rate = 0;
+	static const u16 mcs_rates[2][2][2][8] = {
+		{ // RF_1T1R
+			{ // !bw_40MHz
+				// !short_GI_20
+				{ 65, 130, 195, 260, 390, 520, 585, 650 },
+				// short_GI_20
+				{ 72, 144, 217, 289, 433, 578, 650, 722 },
+			},
+			{ // bw_40MHz
+				// !short_GI_40
+				{ 135, 270, 405, 540, 810, 1080, 1215, 1350 },
+				// short_GI_40
+				{ 150, 300, 450, 600, 900, 1200, 1350, 1500 },
+			},
+		},
+		{ // !RF_1T1R
+			{ // !bw_40MHz
+				// !short_GI_20
+				{ 130, 260, 390, 520, 780, 1040, 1170, 1300 },
+				// short_GI_20
+				{ 144, 289, 433, 578, 867, 1156, 1300, 1444 },
+			},
+			{ // bw_40MHz
+				// !short_GI_40
+				{ 270, 540, 810, 1080, 1620, 2160, 2430, 2700 },
+				// short_GI_40
+				{ 300, 600, 900, 1200, 1800, 2400, 2700, 3000 },
+			},
+		},
+	};
 
-	if(rf_type == RF_1T1R)
-	{
-		if(MCS_rate[0] & BIT(7))
-			max_rate = (bw_40MHz) ? ((short_GI_40)?1500:1350):((short_GI_20)?722:650);
-		else if(MCS_rate[0] & BIT(6))
-			max_rate = (bw_40MHz) ? ((short_GI_40)?1350:1215):((short_GI_20)?650:585);
-		else if(MCS_rate[0] & BIT(5))
-			max_rate = (bw_40MHz) ? ((short_GI_40)?1200:1080):((short_GI_20)?578:520);
-		else if(MCS_rate[0] & BIT(4))
-			max_rate = (bw_40MHz) ? ((short_GI_40)?900:810):((short_GI_20)?433:390);
-		else if(MCS_rate[0] & BIT(3))
-			max_rate = (bw_40MHz) ? ((short_GI_40)?600:540):((short_GI_20)?289:260);
-		else if(MCS_rate[0] & BIT(2))
-			max_rate = (bw_40MHz) ? ((short_GI_40)?450:405):((short_GI_20)?217:195);
-		else if(MCS_rate[0] & BIT(1))
-			max_rate = (bw_40MHz) ? ((short_GI_40)?300:270):((short_GI_20)?144:130);
-		else if(MCS_rate[0] & BIT(0))
-			max_rate = (bw_40MHz) ? ((short_GI_40)?150:135):((short_GI_20)?72:65);
-	}
-	else
-	{
-		if(MCS_rate[1])
-		{
-			if(MCS_rate[1] & BIT(7))
-				max_rate = (bw_40MHz) ? ((short_GI_40)?3000:2700):((short_GI_20)?1444:1300);
-			else if(MCS_rate[1] & BIT(6))
-				max_rate = (bw_40MHz) ? ((short_GI_40)?2700:2430):((short_GI_20)?1300:1170);
-			else if(MCS_rate[1] & BIT(5))
-				max_rate = (bw_40MHz) ? ((short_GI_40)?2400:2160):((short_GI_20)?1156:1040);
-			else if(MCS_rate[1] & BIT(4))
-				max_rate = (bw_40MHz) ? ((short_GI_40)?1800:1620):((short_GI_20)?867:780);
-			else if(MCS_rate[1] & BIT(3))
-				max_rate = (bw_40MHz) ? ((short_GI_40)?1200:1080):((short_GI_20)?578:520);
-			else if(MCS_rate[1] & BIT(2))
-				max_rate = (bw_40MHz) ? ((short_GI_40)?900:810):((short_GI_20)?433:390);
-			else if(MCS_rate[1] & BIT(1))
-				max_rate = (bw_40MHz) ? ((short_GI_40)?600:540):((short_GI_20)?289:260);
-			else if(MCS_rate[1] & BIT(0))
-				max_rate = (bw_40MHz) ? ((short_GI_40)?300:270):((short_GI_20)?144:130);
-		}
-		else
-		{
-			if(MCS_rate[0] & BIT(7))
-				max_rate = (bw_40MHz) ? ((short_GI_40)?1500:1350):((short_GI_20)?722:650);
-			else if(MCS_rate[0] & BIT(6))
-				max_rate = (bw_40MHz) ? ((short_GI_40)?1350:1215):((short_GI_20)?650:585);
-			else if(MCS_rate[0] & BIT(5))
-				max_rate = (bw_40MHz) ? ((short_GI_40)?1200:1080):((short_GI_20)?578:520);
-			else if(MCS_rate[0] & BIT(4))
-				max_rate = (bw_40MHz) ? ((short_GI_40)?900:810):((short_GI_20)?433:390);
-			else if(MCS_rate[0] & BIT(3))
-				max_rate = (bw_40MHz) ? ((short_GI_40)?600:540):((short_GI_20)?289:260);
-			else if(MCS_rate[0] & BIT(2))
-				max_rate = (bw_40MHz) ? ((short_GI_40)?450:405):((short_GI_20)?217:195);
-			else if(MCS_rate[0] & BIT(1))
-				max_rate = (bw_40MHz) ? ((short_GI_40)?300:270):((short_GI_20)?144:130);
-			else if(MCS_rate[0] & BIT(0))
-				max_rate = (bw_40MHz) ? ((short_GI_40)?150:135):((short_GI_20)?72:65);
-		}
-	}
-	return max_rate;
+	if (rf_type != RF_1T1R && MCS_rate[1])
+		return mcs_rates[1][!!bw_40MHz][!!(bw_40MHz ? short_GI_40 : short_GI_20)][__fls(MCS_rate[1])];
+
+	if (!MCS_rate[0])
+		return 0;
+	return mcs_rates[0][!!bw_40MHz][!!(bw_40MHz ? short_GI_40 : short_GI_20)][__fls(MCS_rate[0])];
 }
 
 int rtw_action_frame_parse(const u8 *frame, u32 frame_len, u8* category, u8 *action)
